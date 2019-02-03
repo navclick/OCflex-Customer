3a9fac69b1203d72d08767f685b92e6f085e88a4
irfan <asa_khann@hotmail.com>
2019-02-03 17:20:52 +0500

    permissions
    permissions

--- app/src/main/java/com/example/fightersarena/ocflex_costumer/Helpers/Constants.java
+++ app/src/main/java/com/example/fightersarena/ocflex_costumer/Helpers/Constants.java
@@ -61,4 +61,5 @@ public class Constants {
     public static final String MSG_SERVICE_STATUS_UPDATE_FAILED= "Service status updatedion failed!";
     public static final String MESSAGE_REQUESTED_PERMISSION_DENIED = "Requested permissions required to continue";
 
+
 }
