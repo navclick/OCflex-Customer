33560a4f3d3be61c47c51cb67393c394307577f4
Navclick <naveed2309923@gmail.com>
2019-02-04 19:10:24 +0500

    Customer Experience

---
+++ app/src/main/java/com/example/fightersarena/ocflex_costumer/Activities/CustomerExperienceActivity.java
@@ -0,0 +1,15 @@
+package com.example.fightersarena.ocflex_costumer.Activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.fightersarena.ocflex_costumer.R;
+
+public class CustomerExperienceActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_customer_experience);
+    }
+}
