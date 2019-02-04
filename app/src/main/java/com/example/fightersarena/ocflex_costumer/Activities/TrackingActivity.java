2bcea3b3a0f32f2b917283664c2736dff8886dc0
irfan <asa_khann@hotmail.com>
2019-02-04 15:48:31 +0500

    update

--- app/src/main/java/com/example/fightersarena/ocflex_costumer/Activities/TrackingActivity.java
+++ app/src/main/java/com/example/fightersarena/ocflex_costumer/Activities/TrackingActivity.java
@@ -1,18 +1,191 @@
 package com.example.fightersarena.ocflex_costumer.Activities;
 
 import android.os.Bundle;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.MenuItemCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 import com.example.fightersarena.ocflex_costumer.Base.BaseActivity;
 import com.example.fightersarena.ocflex_costumer.R;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.squareup.picasso.Picasso;
 
-public class TrackingActivity extends BaseActivity {
-
-
+public class TrackingActivity extends BaseActivity implements OnMapReadyCallback,NavigationView.OnNavigationItemSelectedListener {
+    public TextView tv;
+    public ImageView i;
+    private GoogleMap mMap;
+    static final LatLng HAMBURG = new LatLng(53.558, 9.927);
+    static final LatLng KIEL = new LatLng(53.551, 9.993);
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.login);
+        setContentView(R.layout.activity_tracking);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar_tracking);
+        setSupportActionBar(toolbar);
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout_tracking);
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.setDrawerListener(toggle);
+        toggle.syncState();
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view_tracking);
+        navigationView.setNavigationItemSelectedListener(this);
+
+        //-----------------------------------
+
+//Show pic and name on drawer menu
+
+        View header = navigationView.getHeaderView(0);
+        TextView t = (TextView) header.findViewById(R.id.txt_main_name);
+        TextView tEmail = (TextView) header.findViewById(R.id.txt_email);
+        ImageView profile_img= (ImageView) header.findViewById(R.id.img_nav_profile);
+        tEmail.setText(tokenHelper.GetUserEmail());
+
+        t.setText(tokenHelper.GetUserName());
+
+        //profile_img.setBackground(getResources().getDrawable(R.drawable.profile_image_border));
+        Picasso.with(this).load(tokenHelper.GetUserPhoto()).resize(110, 110).centerCrop().into(profile_img);
+
+
+
+
+
+
+
+        ///--------
+
+
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+
+
+
+
     }
+
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        // Add a marker in Sydney and move the camera
+
+       // LatLng custLocation = getLocationFromAddress(this,OrderDetails.CustomerAddress);
+
+
+        LatLng custLocation= HAMBURG;
+        //Log.d(Constants.TAG,"cusLoc"+ String.valueOf(custLocation.latitude));
+        //  mMap.animateCamera( CameraUpdateFactory.zoomTo( 17.0f ) );
+        float zoomLevel = (float) 15.0;
+
+        MarkerOptions markerOptions = new MarkerOptions();
+
+        // Setting the position for the marker
+        markerOptions.position(custLocation);
+
+        // Setting the title for the marker.
+        // This will be displayed on taping the marker
+//        markerOptions.title(OrderDetails.CustomerName);
+
+        markerOptions.title("test");
+
+        // Clears the previously touched position
+
+
+
+        mMap.addMarker(markerOptions).showInfoWindow();
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(custLocation,zoomLevel));
+
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+        DrawerLayout mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout_tracking);
+
+        if (id == R.id.my_orders) {
+            // Handle the camera action
+            mDrawerLayout.closeDrawers();
+            // openActivityWithFinish(AboutActivity.class);
+            BaseActivity.startActivity(this,MyOrderActivity.class);
+
+        }  else if (id == R.id.menu_profile) {
+            mDrawerLayout.closeDrawers();
+            BaseActivity.startActivity(this,EditProfileActivity.class);
+            // OpenActivity(EditProfileActivity.class);
+            //openActivityProfile();
+            //MenuHandler.smsTracking(this);
+            //MenuHandler.callUs(this);
+            //ActivityManager.showPopup(BookingActivity.this, Constant.CALL_NOW_DESCRIPTION, Constant.CALL_NOW_HEADING, Constant.CANCEL_BUTTON, Constant.CALL_NOW_BUTTON, Constant.CALL_BUTTON, Constant.PopupType.INFORMATION.ordinal());
+        }
+
+        else if (id == R.id.menu_all_setting) {
+            mDrawerLayout.closeDrawers();
+            BaseActivity.startActivity(this,SettingActivity.class);
+            // openActivity(ShoppingListActivity.class);
+            //MenuHandler.smsTracking(this);
+            //MenuHandler.callUs(this);
+            //ActivityManager.showPopup(BookingActivity.this, Constant.CALL_NOW_DESCRIPTION, Constant.CALL_NOW_HEADING, Constant.CANCEL_BUTTON, Constant.CALL_NOW_BUTTON, Constant.CALL_BUTTON, Constant.PopupType.INFORMATION.ordinal());
+        }
+
+        else if (id == R.id.menu_service) {
+            mDrawerLayout.closeDrawers();
+            BaseActivity.startActivity(this,ServicesListActivity.class);
+
+            // openActivity(AllCatActivity.class);
+
+            //MenuHandler.smsTracking(this);
+            //MenuHandler.callUs(this);
+            //ActivityManager.showPopup(BookingActivity.this, Constant.CALL_NOW_DESCRIPTION, Constant.CALL_NOW_HEADING, Constant.CANCEL_BUTTON, Constant.CALL_NOW_BUTTON, Constant.CALL_BUTTON, Constant.PopupType.INFORMATION.ordinal());
+
+        } else if (id == R.id.menu_pro_logout) {
+            mDrawerLayout.closeDrawers();
+            // openActivity(AllCatActivity.class);
+
+            //MenuHandler.smsTracking(this);
+            //MenuHandler.callUs(this);
+            //ActivityManager.showPopup(BookingActivity.this, Constant.CALL_NOW_DESCRIPTION, Constant.CALL_NOW_HEADING, Constant.CANCEL_BUTTON, Constant.CALL_NOW_BUTTON, Constant.CALL_BUTTON, Constant.PopupType.INFORMATION.ordinal());
+            logOut();
+        }
+
+        return  true;
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.toolbarmenu, menu);
+        MenuItem item = menu.findItem(R.id.badge);
+        MenuItemCompat.setActionView(item, R.layout.menu_cart);
+        RelativeLayout notifCount = (RelativeLayout)   MenuItemCompat.getActionView(item);
+        i =notifCount.findViewById(R.id.actionbar_notifcation_img);
+        tv = (TextView) notifCount.findViewById(R.id.actionbar_notifcation_textview);
+        //tv.setText("12");
+        tv.setText("0");
+        //   i.setOnClickListener(this);
+        //  tv.setOnClickListener(this);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+
 
 
 }
