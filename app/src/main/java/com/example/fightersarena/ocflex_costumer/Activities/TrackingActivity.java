760686145ca5f3b2f2ee7f6e071a4b590b8ee963
irfan <asa_khann@hotmail.com>
2019-02-04 15:12:48 +0500

    update

---
+++ app/src/main/java/com/example/fightersarena/ocflex_costumer/Activities/TrackingActivity.java
@@ -0,0 +1,18 @@
+package com.example.fightersarena.ocflex_costumer.Activities;
+
+import android.os.Bundle;
+
+import com.example.fightersarena.ocflex_costumer.Base.BaseActivity;
+import com.example.fightersarena.ocflex_costumer.R;
+
+public class TrackingActivity extends BaseActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login);
+    }
+
+
+}
